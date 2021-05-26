c232693823c19b239107d90d9c468b70cf2d190b
Mikołaj Przenzak <mprzenzak@gmail.com>
2021-05-26 11:49:10 +0200

    Service file tested and updated

--- src/main/java/Services.java
+++ src/main/java/Services.java
@@ -1,8 +1,6 @@
-package main.java;
-
 public class Services {
     public static void main(String[] args) {
-        System.out.println("Spierdalaj");
+        System.out.println("test");
     }
 }
 
