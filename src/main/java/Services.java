ab310e3e904b4e124aac7aeb693b89e99e86d19b
Mikołaj Przenzak <mprzenzak@gmail.com>
2021-05-26 12:26:41 +0200

    Initial parameters

--- src/main/java/Services.java
+++ src/main/java/Services.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Services {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Witaj w symulacji agentowej przedstawiającej kolonię blobków \nPodaj parametry wejściowe:");
+//        System.out.println("Początkowa ilość blobków:");
+//        Scanner sc = new Scanner(System.in);
+//        int initialBlobsAmount = sc.nextInt();
+//        System.out.println("Ilość jedzenia:");
+//        int initialFoodAmount = sc.nextInt();
+//        System.out.println("Czas trwania symulacji (dni):");
+//        int simulationLength = sc.nextInt();
+//        System.out.println("Stosunek liczby altruistow do agresorów, np. 60/40:");
+//        String blobsProportion = sc.nextLine();
+//
+//        System.out.println(initialBlobsAmount);
+//        System.out.println(initialFoodAmount);
+//        System.out.println(simulationLength);
+//        System.out.println(blobsProportion);
     }
 }
 
